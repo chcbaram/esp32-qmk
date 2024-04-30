3c8159d242a94171ab9f16cfb65a0223e52aad11
Hancheol Cho <chcbaram@gmail.com>
2024-04-30 16:16:44 +0900

    기본 프로젝트 추가

---
+++ firmware/esp32-qmk/main/hw/hw.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_HW_HW_H_
+#define MAIN_HW_HW_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "hw_def.h"
+
+
+#include "uart.h"
+#include "cli.h"
+#include "log.h"
+#include "nvs.h"
+
+
+
+bool hwInit(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
